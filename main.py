7c0442e5cc0d97cb3b2c3bb4552737ec05990c03
Deyvid Lins <deyvidg.lins@gmail.com>
2021-03-22 14:22:29 -0300

    versão 1.0 - Inicial do projeto

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from openpyxl import Workbook # pip install openpyxl
 from openpyxl.styles import Font, Color, colors
+#import pandas as pd
+
+
 
 listDesc=["Descrição"]
 listValor=["Valor"]
@@ -14,6 +17,10 @@ wb = Workbook()
 ws1 = wb.active # work sheet
 ws1.title = "Pyxl"
 
+# Realiza a leitura  do arquvio excel através da biblioteca Pandas
+#x = pd.read_excel(r"C:\Users\Deyvid\Desktop\Repo-GitHub\pythonExcel\ControleFinanceiro.xlsx")
+#print(x)
+
 ws1.cell(column=1, row=1, value=listDesc[0])
 ws1.cell(column=2, row=1, value=listValor[0])
 ws1.cell(column=3, row=1, value=listData[0])
@@ -21,6 +28,7 @@ ws1.cell(column=4, row=1, value=listPag[0])
 ws1.cell(column=5, row=1, value=listValorPag[0])
 ws1.cell(column=6, row=1, value=listDev[0])
 ws1.cell(column=7, row=1, value=listStatus[0])
+
 
 cont = 1
 info = input('Deseja continuar? Digite C para continuar e S para sair:')
@@ -80,6 +88,25 @@ while True:
 ws1["H13"]='TOTAL'
 ws1["I13"]='=SUM(F2:B50)'
 
+
+ft_a = Font(name='Times New Roman',color=colors.BLACK, bold=True, size=12)
+
+a1 = ws1['A1']
+b1 = ws1['B1']
+c1 = ws1['C1']
+d1 = ws1['D1']
+e1 = ws1['E1']
+f1 = ws1['F1']
+g1 = ws1['G1']
+h13 = ws1['H13']
+a1.font = ft_a
+b1.font = ft_a
+c1.font = ft_a
+d1.font = ft_a
+e1.font = ft_a
+f1.font = ft_a
+g1.font = ft_a
+h13.font = ft_a
 
 wb.save('ControleFinanceiro.xlsx')
 
