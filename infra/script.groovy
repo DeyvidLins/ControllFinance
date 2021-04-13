a6ddd3072e69203f5ac217de3d6e603aa71cc717
Deyvid Lins <deyvidg.lins@gmail.com>
2021-04-13 17:03:10 -0300

    alterações

--- infra/script.groovy
+++ infra/script.groovy
@@ -6,8 +6,9 @@ node('master') {
     }
 
     stage("Verificação da qualidade do código via Sonarqube"){
+        withSonarQubeEnv("sonarqube") {
         sh 'sonar-scanner  -Dsonar.projectKey=sonarqube  -Dsonar.sources=.  -Dsonar.host.url=http://localhost:9000  -Dsonar.login=6427ddd395e6d22fd32a90b1ee936c112775e3f4'
-
+        }
     }
     
     stage("Classes de Teste") {
