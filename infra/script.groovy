b665711aa39bcc145e5126146aa201b1734c0b91
Deyvid Lins <deyvidg.lins@gmail.com>
2021-04-13 11:34:05 -0300

    Ajuste- Stage de instalação das Bibliotecas Python

--- infra/script.groovy
+++ infra/script.groovy
@@ -3,6 +3,9 @@ node('master') {
         checkout scm
 
     }
+    stage("Instalação das Bibliotecas Python"){
+        sh 'pip install -r requirements.txt'
+    }
     stage("Classes de Teste") {
         sh 'pytest -vv --cov -W ignore::DeprecationWarning'
     }
