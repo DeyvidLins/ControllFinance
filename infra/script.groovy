e4bfab83950b24c3c273709181bbaf0eae3cfee2
Deyvid Lins <deyvidg.lins@gmail.com>
2021-04-13 17:09:47 -0300

    alterações

--- infra/script.groovy
+++ infra/script.groovy
@@ -7,7 +7,7 @@ node('master') {
 
     stage("Verificação da qualidade do código via Sonarqube"){
         withSonarQubeEnv("sonarqube") {
-        sh 'sonar-scanner  -Dsonar.projectKey=sonarqube  -Dsonar.sources=.  -Dsonar.host.url=http://host.docker.internal:9000  -Dsonar.login=6427ddd395e6d22fd32a90b1ee936c112775e3f4'
+        sh 'sonar-scanner  -Dsonar.projectKey=sonarqube  -Dsonar.sources=.  -Dsonar.host.url=https://host.docker.internal:9000  -Dsonar.login=6427ddd395e6d22fd32a90b1ee936c112775e3f4'
         }
     }
     
